aad7b14880b465df868a393eb9d778f426b9dbab
mhae03 <maximilian.haeffele@sw-machines.com>
2023-04-13 11:24:54 +0200

    kann man so eigentlich benutzen

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ def get_pokemon_sprite(pokemon):
     return get_json('pokemon/' + pokemon)['sprites']['front_default']
 
 
+def get_image(url):
+    return Image.open(requests.get(url, stream=True).raw)
+
+
 def generate_image_files(team):
     for f in os.listdir("pokemon_pictures"):
         if not f.endswith(".png"):
@@ -26,11 +30,11 @@ def generate_image_files(team):
         os.remove(os.path.join("pokemon_pictures", f))
     counter = 1
     for pokemon in team:
-        img = Image.open(requests.get(get_pokemon_sprite(pokemon.lower()), stream=True).raw)
+        img = get_image(get_pokemon_sprite(pokemon.lower()))
         img.save(f"pokemon_pictures/p{counter}.png")
         counter += 1
 
 
 if __name__ == '__main__':
-    generate_image_files(["Charizard", "Ditto", "Aerodactyl", "Umbreon", "Mudkip"])
+    generate_image_files(["mimikyu-disguised", "gumshoos", "decidueye-hisui"])
     # generate_window.render_team()
