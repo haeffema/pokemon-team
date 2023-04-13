4ff23372295efb022c91503024409f9a6d867bc7
mhae03 <maximilian.haeffele@sw-machines.com>
2023-04-13 10:08:33 +0200

    aktueller zwischen stand

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-import tkinter
-from tkinter import *
-from PIL import Image, ImageTk
+import os
 
-root = Tk()
+import requests
+from PIL import Image
 
-# Create a photoimage object of the image in the path
-image1 = Image.open("ditto.png")
-test = ImageTk.PhotoImage(image1)
 
-label1 = tkinter.Label(image=test)
-label1.image = test
+def get_json(url):
+    return requests.get('https://pokeapi.co/api/v2/' + url).json()
 
-# Position image
-label1.place(x=0, y=0)
-root.mainloop()
+
+def generate_pokemon_list():
+    updated_pokemon_list = []
+    for pokemon in get_json('pokemon?limit=100000&offset=0')['results']:
+        updated_pokemon_list.append(pokemon['name'])
+    return updated_pokemon_list
+
+
+def get_pokemon_sprite(pokemon):
+    return get_json('pokemon/' + pokemon)['sprites']['front_default']
+
+
+def generate_image_files(team):
+    for f in os.listdir("pokemon_pictures"):
+        if not f.endswith(".png"):
+            continue
+        os.remove(os.path.join("pokemon_pictures", f))
+    counter = 1
+    for pokemon in team:
+        img = Image.open(requests.get(get_pokemon_sprite(pokemon.lower()), stream=True).raw)
+        img.save(f"pokemon_pictures/p{counter}.png")
+        counter += 1
+
+
+if __name__ == '__main__':
+    generate_image_files(["Charizard", "Ditto", "Aerodactyl", "Umbreon", "Mudkip"])
+    # generate_window.render_team()
